3a6508d1274bfa0dbbc133f3b7d18b7dbb37cd7c
Marcos Amorim <mamorim@outlook.com>
2025-12-21 21:51:28 +0000

    feat(api_version): adding API_VERSION to it, so it will be consistent with changes implemented

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  API_VERSION: '0',
   SHORTENER_API_BASE_URL: 'https://rdrt.uk',
   AUTH_API_BASE_URL: 'https://auth.rdrt.uk',
   AUTH_TOKEN_PATH: '/auth/login',
